71497704fcefc23020f27511f82378040befadec
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-10-29 17:43:58 +0600

    Post app update

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('title','updated', 'timestamp',)
+    list_display_links = ["updated"]
+    list_filter = ['updated','timestamp']
+    search_fields = ['title','content']
+    class Meta: #his is just a class container with some options (metadata) attached to the model.
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+from .models import Post
+
+def post_home(request):
+    return HttpResponse("<h1>This is home</h1>")
+
+def post_detail(request):
+
+    context = {
+        'title': "details",
+        'content': "this is post details"
+    }
+    return render(request, "index.html", context)
+def post_list(request):
+    qs = Post.objects.all()
+    context = {
+        'title': "list",
+        'object_list': qs
+    }
+    return render(request, "index.html", context)
+def post_delete(request):
+    return HttpResponse("<h1>This is delete</h1>")
