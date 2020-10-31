bef9d7d0f029b88483497edf516d7e95d027dd5c
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-10-31 10:16:41 +0600

    un solve able contact form

--- DjangoBlog/views.py
+++ DjangoBlog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render,HttpResponse, get_object_or_404, HttpResponseRedirect
 
 def my_home(request):
+    print(request.POST)
+    name = request.POST.get('name')
+    email= request.POST.get('email')
+    subject= request.POST.get('subject')
+    textarea= request.POST.get('textarea')
+
+    print(name)
+
     return render(request,'My_Home.html')
