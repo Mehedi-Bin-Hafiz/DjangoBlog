833e67e831cb2fb91e5561324241fa6b2b8e5452
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-11-01 10:54:56 +0600

    exception handling

--- DjangoBlog/views.py
+++ DjangoBlog/views.py
@@ -8,16 +8,18 @@ def my_home(request):
     import getpass
     from uuid import getnode as get_mac
     import re, uuid
-    import socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     name = request.GET.get('name')
     email= request.GET.get('email')
     subject= request.GET.get('subject')
     message= request.GET.get('textarea')
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    user = getpass.getuser()
+    try:
+
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        user = getpass.getuser()
+    except:
+        local_ip ='unknown'
+        user ='Android'
     send_mail(str(subject),
               ( str(message) + ' \n\nPerson name: ' + str(name) + '\nMail :' + str(email)+'\n local ip:'+local_ip+'\n mac address: '+str(':'.join(re.findall('..', '%012x' % uuid.getnode())))+"\nPc username is: "+str(user)), EMAIL_HOST_USER,
                   ['mehedibinhafiz@gmail.com'], fail_silently=False)
